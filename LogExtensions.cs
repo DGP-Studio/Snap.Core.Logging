8bb2e71e78f721500e0e1b775d2048769f36131f
DismissedLight <1686188646@qq.com>
2022-03-31 22:35:15 +0800

    refactor this. style

--- LogExtensions.cs
+++ LogExtensions.cs
@@ -5,15 +5,25 @@ using System.Runtime.CompilerServices;
 
 namespace Snap.Core.Logging
 {
-    public static partial class ObjectExtensions
+    /// <summary>
+    /// 日志记录
+    /// </summary>
+    public static partial class LogExtensions
     {
         /// <summary>
         /// 记录日志
         /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
         /// <param name="obj">记录日志的调用类</param>
-        /// <param name="info">要记录入日志的信息</param>
-        /// <param name="formatter">格式化输入</param>
-        public static void Log<T>(this T obj, object? info, Func<object?, string>? formatter = null,
+        /// <param name="info">信息</param>
+        /// <param name="formatter">格式化器</param>
+        /// <param name="callerMemberName">调用名称</param>
+        /// <param name="callerLineNumber">调用行号</param>
+        /// <param name="callerFilePath">调用文件</param>
+        public static void Log<T>(
+            this T obj,
+            object? info,
+            Func<object?, string>? formatter = null,
             [CallerMemberName] string? callerMemberName = null,
             [CallerLineNumber] int? callerLineNumber = null,
             [CallerFilePath] string? callerFilePath = null)
@@ -21,6 +31,13 @@ namespace Snap.Core.Logging
             Logger.Instance.LogInternal(info, formatter, callerMemberName, callerLineNumber, callerFilePath);
         }
 
+        /// <summary>
+        /// 将信息写入桌面上的文件
+        /// </summary>
+        /// <typeparam name="T">对象类型</typeparam>
+        /// <param name="obj">对象</param>
+        /// <param name="info">信息</param>
+        /// <param name="fileName">文件名称</param>
         [Conditional("DEBUG")]
         public static void WriteToDesktopFile<T>(this T obj, string? info, string fileName)
         {
--- Logger.cs
+++ Logger.cs
@@ -5,55 +5,53 @@ using System.Runtime.CompilerServices;
 
 namespace Snap.Core.Logging
 {
+    /// <summary>
+    /// 日志记录器
+    /// </summary>
     public class Logger
     {
+        private static volatile Logger? instance;
+
+        [SuppressMessage("", "IDE0044")]
+        private static object locker = new();
+
         private string? lastCallerFilePath;
         private string? lastCallerMemberName;
 
-        /// <summary>
-        /// 核心日志方法
-        /// </summary>
-        /// <param name="info"></param>
-        /// <param name="formatter"></param>
-        /// <param name="callerMemberName"></param>
-        /// <param name="callerLineNumber"></param>
-        /// <param name="callerFilePath"></param>
-        internal void LogInternal(object? info, Func<object?, string>? formatter = null,
-            string? callerMemberName = null, int? callerLineNumber = null, string? callerFilePath = null)
+        private Logger()
         {
-            //pre format string
-            if (formatter != null)
-            {
-                info = formatter.Invoke(info);
-            }
-            //trim callerFilePath
-            if (callerFilePath is not null)
-            {
-                int pos = callerFilePath.IndexOf("Snap.Genshin", StringComparison.Ordinal);
-                callerFilePath = callerFilePath[pos..];
-            }
-
-            if (callerMemberName == this.lastCallerMemberName && callerFilePath == this.lastCallerFilePath)
-            {
-                Debug.WriteLine($"[Line:{callerLineNumber,6}] {info}");
-            }
-            else
-            {
-                string log = $"{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss} | {callerFilePath} | {callerMemberName} |\n[Line:{callerLineNumber,6}] {info}";
-
-                if (this.lastCallerFilePath != callerFilePath)
-                {
-                    Debug.Write('\n');
-                }
-
-                Debug.WriteLine(log);
-            }
-
-            this.lastCallerMemberName = callerMemberName;
-            this.lastCallerFilePath = callerFilePath;
         }
 
-        public static void LogStatic(object? info, Func<object?, string>? formatter = null,
+        /// <summary>
+        /// 获取日志记录器的唯一实例
+        /// </summary>
+        internal static Logger Instance
+        {
+            get
+            {
+                if (instance is null)
+                {
+                    lock (locker)
+                    {
+                        instance ??= new();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// 静态的记录信息
+        /// </summary>
+        /// <param name="info">信息</param>
+        /// <param name="formatter">格式化器</param>
+        /// <param name="callerMemberName">调用名称</param>
+        /// <param name="callerLineNumber">调用行号</param>
+        /// <param name="callerFilePath">调用文件</param>
+        public static void LogStatic(
+            object? info,
+            Func<object?, string>? formatter = null,
             [CallerMemberName] string? callerMemberName = null,
             [CallerLineNumber] int? callerLineNumber = null,
             [CallerFilePath] string? callerFilePath = null)
@@ -61,26 +59,52 @@ namespace Snap.Core.Logging
             Instance.LogInternal(info, formatter, callerMemberName, callerLineNumber, callerFilePath);
         }
 
-        #region 单例
-        private static volatile Logger? instance;
-        [SuppressMessage("", "IDE0044")]
-        private static object _locker = new();
-        private Logger() { }
-        public static Logger Instance
+        /// <summary>
+        /// 核心日志方法
+        /// </summary>
+        /// <param name="info">信息</param>
+        /// <param name="formatter">格式化器</param>
+        /// <param name="callerMemberName">调用名称</param>
+        /// <param name="callerLineNumber">调用行号</param>
+        /// <param name="callerFilePath">调用文件</param>
+        internal void LogInternal(
+            object? info,
+            Func<object?, string>? formatter = null,
+            string? callerMemberName = null,
+            int? callerLineNumber = null,
+            string? callerFilePath = null)
         {
-            get
+            // pre format string
+            if (formatter != null)
             {
-                if (instance is null)
-                {
-                    lock (_locker)
-                    {
-                        instance ??= new();
-                    }
-                }
-                return instance;
+                info = formatter.Invoke(info);
             }
+
+            // trim callerFilePath
+            if (callerFilePath is not null)
+            {
+                int pos = callerFilePath.IndexOf("Snap.Genshin", StringComparison.Ordinal);
+                callerFilePath = callerFilePath[pos..];
+            }
+
+            if (callerMemberName == lastCallerMemberName && callerFilePath == lastCallerFilePath)
+            {
+                Debug.WriteLine($"[Line:{callerLineNumber,6}] {info}");
+            }
+            else
+            {
+                string log = $"{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss} | {callerFilePath} | {callerMemberName} |\n[Line:{callerLineNumber,6}] {info}";
+
+                if (lastCallerFilePath != callerFilePath)
+                {
+                    Debug.Write('\n');
+                }
+
+                Debug.WriteLine(log);
+            }
+
+            lastCallerMemberName = callerMemberName;
+            lastCallerFilePath = callerFilePath;
         }
-        #endregion
     }
 }
-
